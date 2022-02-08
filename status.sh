371571b33e11422d133f080932f8360bfa162973
eunowa <50851693+eunowa@users.noreply.github.com>
2022-02-08 17:48:38 +0900

    status.sh

--- status.sh
+++ status.sh
@@ -3,4 +3,4 @@
 
 echo "===  Process Check                                        ==="
 echo "============================================================="
-ps -ef | grep java | grep "DDOMAIN_GROUP_NAME=$DOMAIN_GROUP_NAME " | grep "SERVER=$SERVER_NAME "
+ps -ef | grep java | grep "DDOMAIN_GROUP_NAME=$DOMAIN_GROUP_NAME " | grep "SERVER=$SERVER_NAME"
