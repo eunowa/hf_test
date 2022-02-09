0a8e7d0d91cb9af0a44f95fc11b6f3ac6423eb7c
eunowa <50851693+eunowa@users.noreply.github.com>
2022-02-09 11:37:21 +0900

    start.sh

---
+++ httpd_bin/start.sh
@@ -0,0 +1,18 @@
+#/bin/sh
+HTTPD_HOME=$HOME/httpd
+OPTIONS="-f $HTTPD_HOME/conf/httpd.conf"
+PATH=$HTTPD_HOME/sbin:$PATH
+LD_LIBRARY_PATH=$HTTPD_HOME/lib:$LD_LIBRARY_PATH
+export LD_LIBRARY_PATH HTTPD_HOME PATH
+if [ `ps -ef | grep "/sbin/httpd" | grep "root" | grep -v " grep" | wc -l` -eq 0 ]; then
+    sudo $HTTPD_HOME/sbin/apachectl ${OPTIONS} -k start
+    sleep 1
+    if [ `ps -ef | grep httpd | grep -v "grep" | wc -l` -ge 1 ]; then
+          echo -e "\nApache Status = $(ps -ef | grep "/sbin/httpd" | grep "root" | grep -v grep | awk '{print "\tPID : " $2, "\tLocation : "$8}')"
+          echo -e "httpd process is running.\n"
+    else
+          echo -e "\nhttpd process is not running.\n"
+    fi
+else
+    echo -e "\nhttpd process is running.\n"
+fi
