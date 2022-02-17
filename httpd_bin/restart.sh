908dee09501cfb0a88bb80731e94826fae724309
eunowa <50851693+eunowa@users.noreply.github.com>
2022-02-17 12:27:23 +0900

    restart.sh

--- httpd_bin/restart.sh
+++ httpd_bin/restart.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
 HTTPD_HOME=$HOME/httpd
 OPTIONS="-f $HTTPD_HOME/conf/httpd.conf"
-PATH=$HTTPD_HOME/sbin:$PATH
+PATH=$HTTPD_HOME/bin:$PATH
 LD_LIBRARY_PATH=$HTTPD_HOME/lib:$LD_LIBRARY_PATH
 export LD_LIBRARY_PATH HTTPD_HOME PATH
-if [ `ps -ef | grep "/sbin/httpd" | grep "root" | grep -v "grep" | wc -l` -ge 1 ]; then
-    sudo $HTTPD_HOME/sbin/apachectl ${OPTIONS} -k graceful
+if [ `ps -ef | grep "/bin/httpd" | grep "root" | grep -v "grep" | wc -l` -ge 1 ]; then
+    sudo $HTTPD_HOME/bin/apachectl ${OPTIONS} -k graceful
     sleep 1
-    echo -e "\nApache Status = $(ps -ef | grep "/sbin/httpd" | grep "root" | grep -v grep | awk '{print "\tPID : " $2, "\tLocation : "$8}')"
+    echo -e "\nApache Status = $(ps -ef | grep "/bin/httpd" | grep "root" | grep -v grep | awk '{print "\tPID : " $2, "\tLocation : "$8}')"
     echo -e "httpd processis restarting.\n"
 else
     echo -e "\nhttpd process is not running.\n"
