7d5fa57278e6f38e643fa4f221bf9d380b970ade
eunowa <50851693+eunowa@users.noreply.github.com>
2022-02-17 12:27:56 +0900

    start.sh

--- httpd_bin/start.sh
+++ httpd_bin/start.sh
@@ -1,14 +1,14 @@
 #/bin/sh
 HTTPD_HOME=$HOME/httpd
 OPTIONS="-f $HTTPD_HOME/conf/httpd.conf"
-PATH=$HTTPD_HOME/sbin:$PATH
+PATH=$HTTPD_HOME/bin:$PATH
 LD_LIBRARY_PATH=$HTTPD_HOME/lib:$LD_LIBRARY_PATH
 export LD_LIBRARY_PATH HTTPD_HOME PATH
-if [ `ps -ef | grep "/sbin/httpd" | grep "root" | grep -v " grep" | wc -l` -eq 0 ]; then
-    sudo $HTTPD_HOME/sbin/apachectl ${OPTIONS} -k start
+if [ `ps -ef | grep "/bin/httpd" | grep "root" | grep -v " grep" | wc -l` -eq 0 ]; then
+    sudo $HTTPD_HOME/bin/apachectl ${OPTIONS} -k start
     sleep 1
     if [ `ps -ef | grep httpd | grep -v "grep" | wc -l` -ge 1 ]; then
-          echo -e "\nApache Status = $(ps -ef | grep "/sbin/httpd" | grep "root" | grep -v grep | awk '{print "\tPID : " $2, "\tLocation : "$8}')"
+          echo -e "\nApache Status = $(ps -ef | grep "/bin/httpd" | grep "root" | grep -v grep | awk '{print "\tPID : " $2, "\tLocation : "$8}')"
           echo -e "httpd process is running.\n"
     else
           echo -e "\nhttpd process is not running.\n"
